4e48570271bb56f4292d7c77253051d67f2aefc8
Jitendra Selvam <jselvam@iu.edu>
2018-11-08 14:10:13 -0500

    added dependencies

---
+++ myvenv/Scripts/coverage-3.7-script.py
@@ -0,0 +1,12 @@
+#!c:\users\jselvam\desktop\twilioapi_newyorktimesnews\myvenv\scripts\python.exe
+# EASY-INSTALL-ENTRY-SCRIPT: 'coverage==4.3.4','console_scripts','coverage-3.7'
+__requires__ = 'coverage==4.3.4'
+import re
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.argv[0] = re.sub(r'(-script\.pyw?|\.exe)?$', '', sys.argv[0])
+    sys.exit(
+        load_entry_point('coverage==4.3.4', 'console_scripts', 'coverage-3.7')()
+    )
